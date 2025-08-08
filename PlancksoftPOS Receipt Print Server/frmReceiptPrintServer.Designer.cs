6c5adf32210290d299a15a058f9f942157ce30fe
Bashar M. Bataineh <basharbataineh@hotmail.com>
2025-08-08 20:52:18 +0300

    Added localization to printing

--- PlancksoftPOS Receipt Print Server/frmReceiptPrintServer.Designer.cs
+++ PlancksoftPOS Receipt Print Server/frmReceiptPrintServer.Designer.cs
@@ -29,11 +29,18 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReceiptPrintServer));
             this.NewReceiptsFetcher = new System.ComponentModel.BackgroundWorker();
             this.cycleTimer = new System.Windows.Forms.Timer(this.components);
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.pbReceipt = new System.Windows.Forms.PictureBox();
+            this.language = new System.Windows.Forms.NotifyIcon(this.components);
+            this.languageContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.اللغةToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.العربيةToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pbReceipt)).BeginInit();
+            this.languageContextMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // NewReceiptsFetcher
@@ -59,6 +66,43 @@
             this.pbReceipt.TabStop = false;
             this.pbReceipt.UseWaitCursor = true;
             // 
+            // language
+            // 
+            this.language.ContextMenuStrip = this.languageContextMenu;
+            this.language.Icon = ((System.Drawing.Icon)(resources.GetObject("language.Icon")));
+            this.language.Text = "PlancksoftPOS Print Server";
+            this.language.Visible = true;
+            // 
+            // languageContextMenu
+            // 
+            this.languageContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.اللغةToolStripMenuItem});
+            this.languageContextMenu.Name = "languageContextMenu";
+            this.languageContextMenu.Size = new System.Drawing.Size(99, 26);
+            // 
+            // اللغةToolStripMenuItem
+            // 
+            this.اللغةToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.العربيةToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.اللغةToolStripMenuItem.Name = "اللغةToolStripMenuItem";
+            this.اللغةToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.اللغةToolStripMenuItem.Text = "اللغة";
+            // 
+            // العربيةToolStripMenuItem
+            // 
+            this.العربيةToolStripMenuItem.Name = "العربيةToolStripMenuItem";
+            this.العربيةToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.العربيةToolStripMenuItem.Text = "العربية";
+            this.العربيةToolStripMenuItem.Click += new System.EventHandler(this.العربيةToolStripMenuItem_Click);
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
             // ReceiptPrintServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -69,6 +113,7 @@
             this.DoubleBuffered = true;
             this.Enabled = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ReceiptPrintServer";
@@ -79,6 +124,7 @@
             this.UseWaitCursor = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             ((System.ComponentModel.ISupportInitialize)(this.pbReceipt)).EndInit();
+            this.languageContextMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -89,6 +135,11 @@
         private System.Windows.Forms.Timer cycleTimer;
         private System.Drawing.Printing.PrintDocument printDocument1;
         private System.Windows.Forms.PictureBox pbReceipt;
+        private System.Windows.Forms.NotifyIcon language;
+        private System.Windows.Forms.ContextMenuStrip languageContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem اللغةToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem العربيةToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
     }
 }
 
