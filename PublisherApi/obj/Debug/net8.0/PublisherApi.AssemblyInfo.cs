1f89cb601c728e729ca9998d5d252829a16776d7
Bashar M. Bataineh <basharbataineh@hotmail.com>
2025-08-11 12:51:05 +0300

    Fixed a small issue with open cash drawer in desktop app that performs extra unnecessary work.

--- PublisherApi/obj/Debug/net8.0/PublisherApi.AssemblyInfo.cs
+++ PublisherApi/obj/Debug/net8.0/PublisherApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublisherApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c2989c4e30086452776ca0907c566d57cfbbe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b8fb5b7a624ab2fb4642b88798af7b26a4a8202")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublisherApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublisherApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
